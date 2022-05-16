86be4ab0b510cee22b682f5e1cfe71c1c4b081e1
Marcelo Guedes <39092762+guedes84@users.noreply.github.com>
2022-05-16 11:40:02 -0300

    Update netsim-tools-install.sh

--- netsim-tools-install.sh
+++ netsim-tools-install.sh
@@ -1,6 +1,6 @@
 # you can copy/paste and run all this text. Theses steps are idempotence.
 apt update && apt -y upgrade && apt -y autoremove
-# THIS WILL REMOVE YOUR PYTHON3, Ansible and Asible core (to avoid troubles)
+# THIS WILL REMOVE YOUR PYTHON3, Ansible and Asible core (to avoid troubles with others versions)
 apt remove -y python3 ansible ansible-core 
 apt install python3-pip
 pip install --upgrade pip
